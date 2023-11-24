1cfac18ffbdce8ba22e4b999cb678f9e86f30eaa
Kaniek <kaniek99@gmail.com>
2023-11-24 17:40:05 +0100

    feat: add first version of solution for 'Colors' Assignment NOT COMPLETE

--- src/Assignments/Colors/shaders/base_fs.glsl
+++ src/Assignments/Colors/shaders/base_fs.glsl
@@ -1,7 +1,8 @@
 #version 410
 
 layout(location=0) out vec4 vFragColor;
+layout(location=2) in vec4 a_vertex_color_out;
 
 void main() {
-    vFragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    vFragColor = a_vertex_color_out;
 }
