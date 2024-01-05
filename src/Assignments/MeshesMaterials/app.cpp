251ddb01e1196d9e173eeccea4e202df7cd230af
Kaniek <kaniek99@gmail.com>
2024-01-05 17:58:03 +0100

    feat: add usage of meshes and materials

--- src/Assignments/MeshesMaterials/app.cpp
+++ src/Assignments/MeshesMaterials/app.cpp
@@ -13,6 +13,8 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 #include "Application/utils.h"
+#include "Engine/Mesh.h"
+#include "Engine/Material.h"
 
 void SimpleShapeApplication::init() {
     // A utility function that reads the shader sources, compiles them and creates the program object
@@ -25,49 +27,56 @@ void SimpleShapeApplication::init() {
         exit(-1);
     }
 
+    xe::ColorMaterial::init();
+
     set_camera(new Camera);
     set_controler(new CameraControler(camera()));
 
     // A vector containing the x,y,z vertex coordinates for the pyramid.
     std::vector<GLfloat> vertices = {
-        0.5f, -0.5f, 0.0f, 0.22f, 0.59f, 0.51f, 1.0f,
-        0.0f, 0.0f, 1.0f, 0.22f, 0.59f, 0.51f, 1.0f,
-        -0.5f, -0.5f, 0.0f, 0.22f, 0.59f, 0.51f, 1.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.0f,
 
-        -0.5f, -0.5f, 0.0f, 0.69f, 0.64f, 0.59f, 1.0f,
-        0.0f, 0.0f, 1.0f, 0.69f, 0.64f, 0.59f, 1.0f,
-        -0.5f, 0.5f, 0.0f, 0.69f, 0.64f, 0.59f, 1.0f,
+        -0.5f, -0.5f, 0.0f,
+        0.0f, 0.0f, 1.0f,
+        -0.5f, 0.5f, 0.0f,
 
-        -0.5f, 0.5f, 0.0f, 0.45f, 0.58f, 0.68f, 1.0f,
-        0.0f, 0.0f, 1.0f, 0.45f, 0.58f, 0.68f, 1.0f,
-        0.5f, 0.5f, 0.0f, 0.45f, 0.58f, 0.68f, 1.0f,
+        -0.5f, 0.5f, 0.0f,
+        0.0f, 0.0f, 1.0f,
+        0.5f, 0.5f, 0.0f,
 
-        0.5f, 0.5f, 0.0f, 0.33f, 0.48f, 0.58f, 1.0f,
-        0.0f, 0.0f, 1.0f, 0.33f, 0.48f, 0.58f, 1.0f,
-        0.5f, -0.5f, 0.0f, 0.33f, 0.48f, 0.58f, 1.0f,
+        0.5f, 0.5f, 0.0f,
+        0.0f, 0.0f, 1.0f,
+        0.5f, -0.5f, 0.0f,
 
-        0.5f, -0.5f, 0.0f, 0.36f, 0.36f, 0.38f, 1.0f,
-        -0.5f, -0.5f, 0.0f, 0.36f, 0.36f, 0.38f, 1.0f,
-        -0.5f, 0.5f, 0.0f, 0.36f, 0.36f, 0.38f, 1.0f,
-        0.5f, 0.5f, 0.0f, 0.36f, 0.36f, 0.38f, 1.0f
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
+
+        0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
+        0.5f, 0.5f, 0.0f
     };
-
-    // Generating the buffer and loading the vertex data into it.
-    GLuint VBO_vertices, VBO_indexes;
-    glGenBuffers(1, &VBO_vertices); //
-    glBindBuffer(GL_ARRAY_BUFFER, VBO_vertices);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     std::vector<GLushort> indexes = {
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 12, 14, 15
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17
     };
 
-    // Generating the buffer and loading the indexes data into it.
-    glGenBuffers(1, &VBO_indexes);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VBO_indexes);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexes.size() * sizeof(GLushort), indexes.data(), GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    auto pyramid = new xe::Mesh;
+    pyramid->allocate_vertex_buffer(vertices.size() * sizeof(GLfloat), GL_STATIC_DRAW);
+    pyramid->load_vertices(0, vertices.size() * sizeof(GLfloat), vertices.data());
+    pyramid->vertex_attrib_pointer(0, 3, GL_FLOAT, 3 * sizeof(GLfloat), 0);
+    // pyramid->vertex_attrib_pointer(1, 4, GL_FLOAT, 7 * sizeof(GLfloat), 3);
+    pyramid->allocate_index_buffer(indexes.size() * sizeof(GLfloat), GL_STATIC_DRAW);
+    pyramid->load_indices(0, indexes.size() * sizeof(GLfloat), indexes.data());
+
+    pyramid->add_submesh(0, 3, new xe::ColorMaterial({0.22f, 0.59f, 0.51f, 1.0f}));
+    pyramid->add_submesh(3, 6, new xe::ColorMaterial({0.69f, 0.64f, 0.59f, 1.0f}));
+    pyramid->add_submesh(6, 9, new xe::ColorMaterial({0.45f, 0.58f, 0.68f, 1.0f}));
+    pyramid->add_submesh(9, 12, new xe::ColorMaterial({0.33f, 0.48f, 0.58f, 1.0f}));
+    pyramid->add_submesh(12, 18, new xe::ColorMaterial({0.36f, 0.36f, 0.38f, 1.0f}));
+    add_submesh(pyramid);
 
     GLuint UBO_modifier;
     float strength = 0.7f;
@@ -98,26 +107,6 @@ void SimpleShapeApplication::init() {
     glBufferData(GL_UNIFORM_BUFFER, sizeof(glm::mat4), nullptr, GL_STATIC_DRAW);
     glBindBuffer(GL_UNIFORM_BUFFER, 0);
 
-    // This setups a Vertex Array Object (VAO) that  encapsulates
-    // the state of all vertex buffers needed for rendering
-    glGenVertexArrays(1, &vao_);
-
-    glBindVertexArray(vao_);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO_vertices);
-
-    // This indicates that the data for attribute 0 should be read from a vertex buffer.
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-    // and this specifies how the data is layout in the buffer.
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3 * sizeof(GLfloat)));
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VBO_indexes);
-    glBindVertexArray(0);
-    //end of vao "recording"
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     // Setting the background color of the rendering window,
     // I suggest not to use white or black for better debuging.
     glClearColor(0.81f, 0.81f, 0.8f, 1.0f);
@@ -135,12 +124,11 @@ void SimpleShapeApplication::frame() {
     glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), &PVM[0]);
     glBindBuffer(GL_UNIFORM_BUFFER, 0);
 
-    // Binding the VAO will setup all the required vertex buffers.
-    glBindVertexArray(vao_);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
-    glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_SHORT, nullptr);
-    glBindVertexArray(0);
+
+    for (auto m: meshes_)
+        m->draw();
 }
 
 void SimpleShapeApplication::framebuffer_resize_callback(int w, int h) {
